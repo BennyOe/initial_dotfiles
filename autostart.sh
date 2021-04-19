3b2efe75767c22f95d80f4a41a72bf2b0f38f0eb
BennyOe <45036971+BennyOe@users.noreply.github.com>
2021-04-19 22:21:11 +0200

    Add files via upload

---
+++ autostart.sh
@@ -0,0 +1,15 @@
+xrandr -s 1920x1440 &
+dwmblocks & 
+nitrogen --restore &
+picom &
+redshift &
+
+# systray applets
+nm-applet & 
+pa-applet &
+
+# set the delay time for the keyboard
+xset r rate 200 40
+
+# set numblock on
+numlockx on
