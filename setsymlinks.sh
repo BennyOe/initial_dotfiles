f1ee3b052377a37ff483b05e1bb852a513e31554
BennyOe <45036971+BennyOe@users.noreply.github.com>
2021-04-23 18:14:37 +0200

    Update setsymlinks.sh

--- setsymlinks.sh
+++ setsymlinks.sh
@@ -74,6 +74,7 @@ rm -f ~/.config/mimeapps.list
 ln ~/.dotfiles/mimeapps.list ~/.config/mimeapps.list
 
 rm -rf ~/.config/gtk-3.0/
+mkdir ~/.config/gtk-3.0
 ln ~/.dotfiles/settings.ini ~/.config/gtk-3.0
 chmod +x ~/.config/gtk-3.0/settings.ini
 
