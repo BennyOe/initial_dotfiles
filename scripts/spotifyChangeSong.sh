2543ce5b92ff6e3e79b9955ad26eb164c839285a
BennyOe <45036971+BennyOe@users.noreply.github.com>
2021-05-05 19:17:29 +0200

    Update spotifyChangeSong.sh

--- scripts/spotifyChangeSong.sh
+++ scripts/spotifyChangeSong.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-song=$(playerctl metadata --format "Title: {{ title }}\nArtist: {{ artist }}\nAlbum: {{ album }}")
-notify-send "Spotify" "$song" --icon=~/.dotfiles/spotify.png 
+song=$(playerctl metadata --format "{{ artUrl }}\nTitle: {{ title }}\nArtist: {{ artist }}\nAlbum: {{ album }}")
+cover=$(playerctl metadata  | awk '/artUrl/ {print $3}')
+rm ~/.cache/cover.jpg
+curl $cover >> ~/.cache/cover.jpg
+notify-send "Spotify" "$song" --icon=~/.cache/cover.jpg
