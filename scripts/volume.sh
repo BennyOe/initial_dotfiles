b065ffe19097ff886dd81aa7b82f38a9ccf96922
BennyOe <45036971+BennyOe@users.noreply.github.com>
2021-05-07 10:15:51 +0200

    Create volume.sh

---
+++ scripts/volume.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+    muted=`pamixer --get-mute`
+    volume=`pamixer --get-volume`
+    bar=$(seq -s "─" $(($volume / 6)) | sed 's/[0-9]//g')
+    space="             $bar"
+    if [ $muted == true ]; then
+        dunstify -u low -r 101 " Muted"
+    else
+        if [ $volume -le 50 ]; then
+            dunstify -u low -r 101 " $bar $volume"
+        else
+            dunstify -u low -r 101 " $bar $volume"
+        fi
+    fi
