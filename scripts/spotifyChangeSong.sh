b3ca7da93f745694c07652a293d6f817b36a7e26
Benjamin <b.oechsle@web.de>
2021-05-19 15:05:53 +0200

    corrected player for playerctl

--- scripts/spotifyChangeSong.sh
+++ scripts/spotifyChangeSong.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-song=$(playerctl metadata --format "Title: {{ title }}\nArtist: {{ artist }}\nAlbum: {{ album }}")
-cover=$(playerctl metadata  | awk '/artUrl/ {print $3}')
+song=$(playerctl --player=spotifyd metadata --format "Title: {{ title }}\nArtist: {{ artist }}\nAlbum: {{ album }}")
+cover=$(playerctl --player=spotifyd metadata  | awk '/artUrl/ {print $3}')
 curl $cover > ~/.cache/cover.jpg
 dunstify -u low -r 10 "Spotify" "$song" --icon=~/.cache/cover.jpg
