a8e95ae7ae9131cbc0e8c53addb54b02cc81bda5
Benjamin <B.Oechsle@web.de>
2022-01-25 16:08:07 +0100

    moved comm script

--- communications.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-discord &
-signal-desktop &
-whatsapp-for-linux &
---
+++ scripts/communications.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+discord &
+signal-desktop &
+whatsapp-for-linux &
+sleep 1
+xdotool key Super_L+6
+xdotool key Super_L+Control_R+comma
+xdotool key Super_L+Tab
